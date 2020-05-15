2cc8c03da52ddf90414901bc001cb4d2e7123b2d
Brandon Seydel <brandonseydel@hotmail.com>
2020-05-15 00:29:39 -0500

    fix up the stories a little better

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,13 @@
 module.exports = {
     stories: ['../src/**/*.stories.(ts|mdx)'],
     addons: [
-    ],
+        '@storybook/addon-docs',
+        '@storybook/addon-storysource',
+        '@storybook/addon-actions',
+        '@storybook/addon-links',
+        '@storybook/addon-knobs',
+        '@storybook/addon-jest',
+        '@storybook/addon-backgrounds',
+        '@storybook/addon-a11y',
+      ],
   };
--- src/button/button.stories.ts
+++ src/button/button.stories.ts
@@ -1,6 +1,6 @@
 import { MaterialUI } from './../index';
 import { withActions, action } from '@storybook/addon-actions';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { withKnobs, text, select, boolean, radios } from '@storybook/addon-knobs';
 import { StoryFn } from '@storybook/addons';
 import { StoryFnAureliaReturnType, addRegistries, addContainer } from '@storybook/aurelia';
 import { JitHtmlBrowserConfiguration } from 'aurelia';
@@ -12,18 +12,24 @@ type StoryType = StoryFn<Partial<StoryFnAureliaReturnType>> & {
 const container = JitHtmlBrowserConfiguration.createContainer().register(MaterialUI);
 
 export default {
-    title: 'Custom|Custom Elements',
+    title: 'Buttons|Buttons',
     decorators: [withKnobs, withActions, addContainer(container)],
 };
 
 export const SimpleCoolButtonStory: StoryType = () => ({
-    template: `<mwc-button label.bind="type"></mwc-button>`,
+    template: `<mwc-button icon="code" disabled.bind="disabled" unelevated.bind="unelevated" raised.bind="raised" standard label.bind="type">MWC Button</mwc-button>`,
     state: {
-        type: text('label', 'outlined'),
+        standard: boolean('standard', true),
+        unelevated: boolean('unelevated', false),
+        raised: boolean('raised', false),
+        disabled: boolean('disabled', false),
         buttonClick: action('Button Click'),
+        icons: radios('icons', { 'none': '', 'normal': 'normal', 'leading': 'leading' }, '')
     },
+    title: 'asdfasfd'
+
 });
 
 SimpleCoolButtonStory.story = {
-    name: 'Cool Button Story',
+    name: 'Material Button',
 };
